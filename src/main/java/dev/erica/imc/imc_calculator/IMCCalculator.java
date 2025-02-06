9369f02c1c7f61fa17c41a2b6791ca3746adc76f
erikamc99 <erikamc99@gmail.com>
2025-02-06 17:07:53 +0100

    refactored calculateIMC from type Object to type double

--- src/main/java/dev/erica/imc/imc_calculator/IMCCalculator.java
+++ src/main/java/dev/erica/imc/imc_calculator/IMCCalculator.java
@@ -2,11 +2,15 @@ package dev.erica.imc.imc_calculator;
 
 public class IMCCalculator {
 
-    public static Object calculateIMC(double weight, double height) {
+    public static double calculateIMC(double weight, double height) {
         if (weight <= 30 || height <= 1.4) {
             System.out.println("Valores inválidos, por favor consulte con su médico para un correcto cálculo si mide menos de 1,40 m.");
             return -1;
         } return weight / Math.pow(height, 2);
+    }
+
+    public static String classificateIMC(double imc) {
+        
     }
 
 }
